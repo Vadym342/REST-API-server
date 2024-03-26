d6b6cfe179bba5b9b47197f6474acafe7c83f384
Vadym342 <vadimca1.boyarchuc@gmail.com>
2024-03-26 12:05:34 +0200

    feat(RAS-8): run linter

--- server/src/modules/app/app.module.ts
+++ server/src/modules/app/app.module.ts
@@ -2,18 +2,19 @@ import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 
+import { AuthMiddleware } from '@src/middleware/auth.middleware';
+
+import { AuthModule } from '@modules/auth/auth.module';
+import { AuthService } from '@modules/auth/auth.service';
+import { PositionController } from '@modules/position/position.controller';
 import { PositionModule } from '@modules/position/position.module';
+import { UserController } from '@modules/user/user.controller';
 import { UserModule } from '@modules/user/user.module';
 
 import { CommonModule } from '../common/common.module';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from '@modules/auth/auth.module';
-import { AuthService } from '@modules/auth/auth.service';
-import { AuthMiddleware } from '@src/middleware/auth.middleware';
-import { UserController } from '@modules/user/user.controller';
-import { PositionController } from '@modules/position/position.controller';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), CommonModule.forRoot(), UserModule, PositionModule, AuthModule],
