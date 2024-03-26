e272c05ee37a75febb1b7945ac9ff8ad60af3af8
Vadym342 <vadimca1.boyarchuc@gmail.com>
2024-03-26 12:03:02 +0200

    feat(RAS-8): add auth logic

--- server/src/modules/app/app.module.ts
+++ server/src/modules/app/app.module.ts
@@ -1,5 +1,6 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 import { PositionModule } from '@modules/position/position.module';
 import { UserModule } from '@modules/user/user.module';
@@ -8,10 +9,26 @@ import { CommonModule } from '../common/common.module';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from '@modules/auth/auth.module';
+import { AuthService } from '@modules/auth/auth.service';
+import { AuthMiddleware } from '@src/middleware/auth.middleware';
+import { UserController } from '@modules/user/user.controller';
+import { PositionController } from '@modules/position/position.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), CommonModule.forRoot(), UserModule, PositionModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), CommonModule.forRoot(), UserModule, PositionModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthService, JwtService],
 })
-export class AppModule {}
+export class AppModule {
+  public configure(consumer: MiddlewareConsumer): void {
+    consumer
+      .apply(AuthMiddleware)
+      .exclude(
+        { path: 'user', method: RequestMethod.POST },
+        { path: 'user', method: RequestMethod.GET },
+        { path: 'position', method: RequestMethod.GET },
+      )
+      .forRoutes(UserController, PositionController);
+  }
+}
